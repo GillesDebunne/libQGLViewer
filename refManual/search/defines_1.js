c8ba16990f8da740c741be333b1b0b2f98e0c4ef
Gilles Debunne <gilles.debunne@oso-ai.com>
2022-12-29 18:27:12 +0100

    v2.9.1

--- refManual/search/defines_1.js
+++ refManual/search/defines_1.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['qglviewer_5fexport_0',['QGLVIEWER_EXPORT',['../config_8h.html#a26c8f95aa8f060675f2500a3aac614e3',1,'config.h']]],
-  ['qglviewer_5fversion_1',['QGLVIEWER_VERSION',['../config_8h.html#a31753aa03177ca4c55bb1054ceb23107',1,'config.h']]],
-  ['qt_5fclean_5fnamespace_2',['QT_CLEAN_NAMESPACE',['../config_8h.html#a1e9c4d889526b73d9f4d5c688ae88c07',1,'config.h']]]
+  ['qglviewer_5fversion_1',['QGLVIEWER_VERSION',['../config_8h.html#a31753aa03177ca4c55bb1054ceb23107',1,'config.h']]]
 ];
