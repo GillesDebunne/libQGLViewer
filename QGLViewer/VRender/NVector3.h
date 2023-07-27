4dc9899641d01d405c4bd2228d2734971e423397
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2023-07-27 10:15:37 +0200

    NVector3: all operators must be inlined in header.

--- QGLViewer/VRender/NVector3.h
+++ QGLViewer/VRender/NVector3.h
@@ -25,36 +25,26 @@ namespace vrender
     inline double z() const {return _n[2];}
     void setXYZ(double x,double y,double z,bool normalization=true);
 
-    NVector3& operator=(const NVector3& u);
-    /*
-      inline friend bool operator==(const NVector3 &u,const Vector3  &v) {return u.isEqualTo(v);}
-      inline friend bool operator==(const Vector3  &u,const NVector3 &v) {return v.isEqualTo(u);}
-      inline friend bool operator==(const NVector3 &u,const NVector3 &v) {return u.isEqualTo(v);}
-      inline friend bool operator!=(const NVector3 &u,const Vector3  &v) {return !(u == v);}
-      inline friend bool operator!=(const Vector3  &u,const NVector3 &v) {return !(u == v);}
-      inline friend bool operator!=(const NVector3 &u,const NVector3 &v) {return !(u == v);}
-    */
+    NVector3& operator=(const NVector3& u)
+    {
+      if ( &u != this )
+      {
+        _n[0] = u[0];
+        _n[1] = u[1];
+        _n[2] = u[2];
+      }
+      return *this;
+    };
 
-    //inline friend NVector3 operator-(const NVector3 &u) { return NVector3(-u[0],-u[1],-u[2],false); }
-    //inline friend Vector3 operator+(const NVector3 &u,const Vector3  &v);
-    //inline friend Vector3 operator+(const Vector3  &u,const NVector3 &v);
-    //inline friend Vector3 operator+(const NVector3 &u,const NVector3 &v);
-    //inline friend Vector3 operator-(const NVector3 &u,const Vector3  &v);
-    //inline friend Vector3 operator-(const Vector3  &u,const NVector3 &v);
-    //inline friend Vector3 operator-(const NVector3 &u,const NVector3 &v);
-    //friend double operator*(const NVector3 &u,const Vector3  &v);
-    //friend double operator*(const Vector3  &u,const NVector3 &v);
-    //inline friend double operator*(const NVector3 &u,const NVector3 &v);
-    //inline friend Vector3 operator*(double r,const NVector3 &u);
-    //inline friend Vector3 operator/(const NVector3 &u,double r);
-
-    //inline friend Vector3 operator^(const NVector3 &u,const Vector3  &v);
-    //inline friend Vector3 operator^(const Vector3  &u,const NVector3 &v);
-    //inline friend Vector3 operator^(const NVector3 &u,const NVector3 &v);
 
     inline double norm() const {return 1.0;}
     inline double squareNorm() const {return 1.0;}
-    friend std::ostream& operator<<(std::ostream &out,const NVector3 &u);
+
+    friend std::ostream& operator<<(std::ostream &out,const NVector3 &u)
+    {
+      out << u[0] << " " << u[1] << " " << u[2];
+      return out;
+    };
 
     double operator[](int i) const
     {
@@ -62,6 +52,82 @@ namespace vrender
 
       return _n[i];
     }
+
+    inline friend Vector3 operator+(const NVector3 &u,const Vector3  &v)
+    {
+      return Vector3(u[0]+v[0],u[1]+v[1],u[2]+v[2]);
+    };
+
+    inline friend Vector3 operator+(const Vector3  &u,const NVector3 &v)
+    {
+      return Vector3(u[0]+v[0],u[1]+v[1],u[2]+v[2]);
+    };
+
+    inline friend Vector3 operator+(const NVector3 &u,const NVector3 &v)
+    {
+      return Vector3(u[0]+v[0],u[1]+v[1],u[2]+v[2]);
+    };
+
+    inline friend Vector3 operator-(const NVector3 &u,const Vector3  &v)
+    {
+      return Vector3(u[0]-v[0],u[1]-v[1],u[2]-v[2]);
+    };
+
+    inline friend Vector3 operator-(const Vector3  &u,const NVector3 &v)
+    {
+      return Vector3(u[0]-v[0],u[1]-v[1],u[2]-v[2]);
+    };
+
+    inline friend Vector3 operator-(const NVector3 &u,const NVector3 &v)
+    {
+      return Vector3(u[0]-v[0],u[1]-v[1],u[2]-v[2]);
+    };
+
+    inline friend double operator*(const NVector3 &u,const Vector3  &v)
+    {
+      return u[0]*v[0] + u[1]*v[1] + u[2]*v[2];
+    };
+
+    inline friend double operator*(const Vector3  &u,const NVector3 &v)
+    {
+      return u[0]*v[0] + u[1]*v[1] + u[2]*v[2];
+    };
+
+    inline friend double operator*(const NVector3 &u,const NVector3 &v)
+    {
+      return u[0]*v[0] + u[1]*v[1] + u[2]*v[2];
+    };
+
+    inline friend Vector3 operator*(double r,const NVector3 &u)
+    {
+      return Vector3(r*u[0],r*u[1],r*u[2]);
+    };
+
+    inline friend Vector3 operator/(const NVector3 &u,double r)
+    {
+      return Vector3(u[0]/r,u[1]/r,u[2]/r);
+    };
+
+    inline friend Vector3 operator^(const NVector3 &u,const Vector3  &v)
+    {
+      return Vector3( u[1]*v[2]-u[2]*v[1],
+          u[2]*v[0]-u[0]*v[2],
+          u[0]*v[1]-u[1]*v[0]);
+    };
+
+    inline friend Vector3 operator^(const Vector3  &u,const NVector3 &v)
+    {
+      return Vector3( u[1]*v[2]-u[2]*v[1],
+          u[2]*v[0]-u[0]*v[2],
+          u[0]*v[1]-u[1]*v[0]);
+    };
+
+    inline friend Vector3 operator^(const NVector3 &u,const NVector3 &v)
+    {
+      return Vector3( u[1]*v[2]-u[2]*v[1],
+          u[2]*v[0]-u[0]*v[2],
+          u[0]*v[1]-u[1]*v[0]);
+    };
 
   private:
     void normalize();
